49b7723753f96405afca1c861ea1e149bb35bcda
Alfonso <alfonsobanzon@gmail.com>
2023-11-15 00:00:45 -0500

    ex 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,25 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :belongs_to, presence: true
+
+  def validate(record)
+    if record.hourly_rate < 40 || record.hourly_rate > 200
+      record.errors.add :base, "Rate must be between 40 and 200"
+    end
+  end
+
+end
+
+class Store
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {:greater_than => 0}
+end
+
+puts "Store name: "
+new_store_name = gets.chomp
+new_store = Store.new(new_store_name)
